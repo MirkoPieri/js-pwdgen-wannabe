9e22b1945bb5d625e6411a0392b0bddc6a75e949
MirkoPieri <mirko97.pieri@gmail.com>
2019-06-28 16:09:43 +0200

    Creazione vari file, link file js e completamento esercizio.

---
+++ java.js
@@ -0,0 +1,14 @@
+// definizione selettori
+
+var nome, cognome, colorePreferito, passwordGenerated;
+
+nome = prompt( "Inserisci il tuo nome");
+
+cognome = prompt( "Inserisci il tuo cognome");
+
+colorePreferito = prompt( "Inserisci il tuo colore preferito");
+
+passwordGenerated = nome + cognome + colorePreferito + "19";
+
+// definizione testo su div html
+document.getElementById('javagl_id').innerHTML=passwordGenerated;
